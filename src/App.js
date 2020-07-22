3cce3802590cde6a482d5614e8418a6385fbc17b
Alex Hamilton <alexhami916@gmail.com>
2020-07-22 10:34:56 -0600

    Add navbar to all pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from "./pages/Home";
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,37 @@ import "./style.css";
 
 function Navbar() {
     return (
-        <nav>
-            
+        <nav className="navbar row">
+            {/* <div className=""> */}
+            <div className="col-md-6 nav_brand">
+                <img className="ajh" src={process.env.PUBLIC_URL + "/assets/images/logo/ajh_logo_192x192.png"} alt="ajh_logo" />
+                <h2 className="nav_name">
+                    <strong>
+                        Alexander James Hamilton
+                    </strong>
+                </h2>
+                <p className="nav_desc">
+                    Web Developer
+                </p>
+            </div>
+            <div className="col-md-6">
+                <a className="nav_selects" href="/contact">
+                    Resume
+                </a>
+                <div className="nav_splits">
+                    |
+                </div>
+                <a className="nav_selects" href="/contact">
+                    Portfolio
+                </a>
+                <div className="nav_splits">
+                    |
+                </div>
+                <a className="nav_selects" href="/contact">
+                    Contact
+                </a>
+            </div>
+            {/* </div> */}
         </nav>
     )
 };
