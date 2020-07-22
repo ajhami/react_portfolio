c492587b93c788720bd5aaa2a56a9454fa5746f2
Alex Hamilton <alexhami916@gmail.com>
2020-07-21 19:39:21 -0600

    Added routing to app pages.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav>
+            
+        </nav>
+    )
+};
+
+export default Navbar;
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Portfolio() {
+
+    return (
+        <div>
+            Test Portfolio
+        </div>
+    )
+
+};
+
+export default Portfolio;
