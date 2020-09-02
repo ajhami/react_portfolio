26503180c0fcdd8d70947560564e46fa5c6305e5
Alex Hamilton <alexhami916@gmail.com>
2020-09-01 18:41:27 -0600

    Adding compression package

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const compression = require("compression");
 const port = process.env.PORT || 3000;
 
 app.use(compression());
