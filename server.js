630471235183aa2b537c0bb5b31b2bb320119526
Alex Hamilton <alexhami916@gmail.com>
2020-09-04 10:48:38 -0600

    remove publicurl for heroku.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
-  app.get("/", function(req, res) {
+  app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "build/index.html"));
     });
 }
